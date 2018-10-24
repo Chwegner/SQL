455c7f3b079bb48af0287a1d0ba024ce0cfedc36
ChWegner <44315164+Chwegner@users.noreply.github.com>
2018-10-24 17:51:33 +0200

    Update Prozedur_Kundentabelle.sql

--- Prozedur_Kundentabelle.sql
+++ Prozedur_Kundentabelle.sql
@@ -18,17 +18,17 @@ go
 create procedure proc_insert_new_customer
 	    @name    varchar(50),
 	    @vorname varchar(50),
-	    @ort	 varchar(50),
+	    @ort     varchar(50),
 	    @plz     varchar(5)
 as
 
 -- Key erstellen
 declare @key varchar(10)
 set     @key = substring(@name,1,1) + 
-		       substring(@vorname,1,1) +
-		       substring(@ort,1,1)
+	       substring(@vorname,1,1) +
+	       substring(@ort,1,1)
 
--- Key auf Einmaligkeit prüfen / Fortlaufende Nummer anhängen
+-- Key auf Einmaligkeit prÃ¼fen / Fortlaufende Nummer anhÃ¤ngen
 if exists (select kundennr
            from tblKunden
            where kundennr = @key)
@@ -36,7 +36,7 @@ begin
 declare @number varchar
 set @number = (select count(SUBSTRING(kundennr,1,3))
                from tblKunden
-			   where SUBSTRING(kundennr,1,3) = @key)
+	       where SUBSTRING(kundennr,1,3) = @key)
 set @key = @key + '-' + @number
 end
 
